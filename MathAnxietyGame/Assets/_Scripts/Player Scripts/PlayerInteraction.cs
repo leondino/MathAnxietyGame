bfc930b405bf0908343694b62130ee800cdf10bd
Léon Imanuel <leondino0309@gmail.com>
2023-10-13 13:20:21 +0200

    Playerinteraction script now lists all interactableobjects in range and selects the closests one Interacting with emotion blocks now functional (no texts yet)

--- MathAnxietyGame/Assets/_Scripts/Player Scripts/PlayerInteraction.cs
+++ MathAnxietyGame/Assets/_Scripts/Player Scripts/PlayerInteraction.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    [SerializeField]
     private Interactable interactableObject;
+    private List<Interactable> objectsInRange;
 
     /// <summary>
     /// Calls the Interaction within the interactable object
     /// </summary>
     public void InteractWithInteractable()
     {
-        if (interactableObject != null)
+        if (objectsInRange.Count > 0)
+        {
+            interactableObject = CheckClosestToMe();
             if (interactableObject.CanInteract)
                 interactableObject.Interact();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Interactable>())
-            interactableObject = other.GetComponent<Interactable>();
+            objectsInRange.Add(other.GetComponent<Interactable>());
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<Interactable>() && 
-            interactableObject == other.GetComponent<Interactable>())
-            interactableObject = null;
+        if (other.GetComponent<Interactable>() &&
+            objectsInRange.Contains(other.GetComponent<Interactable>()))
+            objectsInRange.Remove(other.GetComponent<Interactable>());
+    }
+
+    /// <summary>
+    /// Checks which interactable object from the interatable objects in range is closest to the player
+    /// </summary>
+    /// <returns>Closest interactable object to player</returns>
+    private Interactable CheckClosestToMe()
+    {
+        Interactable closestInteractable = null;
+        float smallestDistance = 100;
+
+        foreach (Interactable interactable in objectsInRange)
+        {
+            float distance = Vector3.Distance(interactable.transform.position, transform.position);
+            if (distance < smallestDistance)
+            {
+                smallestDistance = distance;
+                closestInteractable = interactable;
+            }
+        }
+
+        return closestInteractable;
     }
 }
