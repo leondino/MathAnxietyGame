979a8c3b6cdd7fe339f00f45e2039504a0245ff6
Léon Imanuel <leondino0309@gmail.com>
2023-10-19 14:31:41 +0200

    Added tooltip box that swicthes positions based on hovering over different strengths Temporary fills text with strength name, but has implementation for localized tooltip text

--- MathAnxietyGame/Assets/_Scripts/UI Scripts/Strenghts/StrengthTooltip.cs
+++ MathAnxietyGame/Assets/_Scripts/UI Scripts/Strenghts/StrengthTooltip.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Localization;
@@ -7,14 +8,36 @@ using UnityEngine.Localization;
 public class StrengthTooltip : MonoBehaviour
 {
     public Strength MyStrength { get; set; }
+    public GameObject toolTipBox;
+    [SerializeField]
+    private float yOffset;
+    private Vector3 myPosition;
 
+    private void Start()
+    {
+        myPosition = transform.position;
+        myPosition.y -= yOffset;
+    }
+
+    /// <summary>
+    /// Set Tooltip box to correct position with correct text
+    /// </summary>
+    /// <param name="eventTrigger"></param>
     public void OnPointerEnter(EventTrigger eventTrigger)
     {
+        toolTipBox.GetComponentInChildren<TextMeshProUGUI>().text = MyStrength.name.GetLocalizedString();
+        toolTipBox.SetActive(true);
+        toolTipBox.transform.position = myPosition;
         Debug.Log("open pop-up");
     }
 
+    /// <summary>
+    /// Hides tooltip box
+    /// </summary>
+    /// <param name="eventTrigger"></param>
     public void OnPointerLeave(EventTrigger eventTrigger)
     {
+        toolTipBox.SetActive(false);
         Debug.Log("close pop-up");
     }
 }
